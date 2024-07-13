714d68d8266526b377d11e54646c1953bb1be036
Gnarwhal <git.aspect893@passmail.net>
2024-07-13 22:26:48 +0000

    Don't generate introspection every build

--- generate_introspection.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-gdbus introspect                         \
-  --xml                                  \
-  --system                               \
-  --dest org.freedesktop.locale1         \
-  --object-path /org/freedesktop/locale1 \
-  > org.freedesktop.locale1.xml
